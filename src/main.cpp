3993353cfe09eb82cd06a3085c09ddcecb89ce47
banban <banbanhandai@gmail.com>
2025-04-21 16:18:24 +0900

    add base function

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
-#include <Arduino.h>
+#include <M5StickCPlus.h>
+#include <BleMouse.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BleMouse bleMouse("BanbanPointer");
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  M5.begin(); 
+  bleMouse.begin();
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(0,0);
+  M5.Lcd.println("Controller");
+  M5.Lcd.println(" BtnA :Next");
+  M5.Lcd.println(" BtnP :Reset");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  M5.update();
+  if (bleMouse.isConnected()) {
+    if (M5.BtnA.wasReleased()) {
+      Serial.println("Send Scroll down");
+      bleMouse.move(0, 0, -1);
+    }
+  
+    if (M5.BtnB.wasReleased()) {
+      Serial.println("Send Scroll up");
+      bleMouse.move(0, 0, 1);
+    }
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
