92f551cd176d5c9686a2c6065998f0f2bd8bafda
banban <banbanhandai@gmail.com>
2025-05-23 00:33:39 +0900

    wifi-setting

---
+++ src/wifi_status.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// please your wifi_status
+inline const char *ssid = "Your SSID";
+inline const char *password = "Your Password";
+inline const char *host_ip = "Your IP Address";
+inline const int host_port = 5005;
+inline const int listen_port = 5009;
